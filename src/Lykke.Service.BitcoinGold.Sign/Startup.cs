e48ac91b363c745c8d7cb5f2dcd17f8e8509b174
giorgi bakuridze <bakuridze13@gmail.com>
2018-09-09 19:11:39 +0400

    LWDEV-8741 Remove BitcoinGold.Service.Sign service configuration

--- src/Lykke.Service.BitcoinGold.Sign/Startup.cs
+++ src/Lykke.Service.BitcoinGold.Sign/Startup.cs
@@ -3,11 +3,9 @@ using System.Threading.Tasks;
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using Lykke.BitcoinGold.Sign.Services;
-using Lykke.Common.Api.Contract.Responses;
 using Lykke.Common.ApiLibrary.Middleware;
 using Lykke.Common.ApiLibrary.Swagger;
 using Lykke.Logs;
-using Lykke.Service.BitcoinGold.Sign.Core.Exceptions;
 using Lykke.Service.BitcoinGold.Sign.Core.Services;
 using Lykke.Service.BitcoinGold.Sign.Core.Settings;
 using Lykke.Service.BitcoinGold.Sign.Modules;
@@ -77,18 +75,7 @@ namespace Lykke.Service.BitcoinGold.Sign
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseLykkeMiddleware("BitcoinGold.Service.Sign", ex =>
-            {
-                if (ex is BusinessException clientError)
-                {
-                    var response = ErrorResponse.Create(clientError.Text);
-                    response.AddModelError(clientError.Code.ToString(), clientError.Text);
-
-                    return response;
-                }
-
-                return new { Message = "Technical problem" };
-            });
+            app.UseLykkeMiddleware(ex => new { Message = "Technical problem" });
 
             app.UseMvc();
             app.UseSwagger();
